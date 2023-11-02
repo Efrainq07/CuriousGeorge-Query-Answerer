5aa368bcb1240c29a18c5b7189cb21fbba376db2
Efrainq07 <efrainq07@hotmail.com>
2023-11-01 20:21:35 -0600

    Added efficient_alpaca imports to __init__ file for use with Fairseq

--- __init__.py
+++ __init__.py
@@ -1,2 +1,7 @@
 from .criterions.MLMetKE import MLMetKELoss
 from .tasks.MLMetKE import MLMetKETask
+from .efficient_alpaca.alpaca.src.model.llama_model import *
+from .efficient_alpaca.alpaca.src.loss.lm_loss import *
+from .efficient_alpaca.alpaca.src.task.seq2seq_ft_task import *
+from .efficient_alpaca.alpaca.src.task.seq2seq_lora_task import *
+from .efficient_alpaca.alpaca.src.fsdp.cpu_adam import *
